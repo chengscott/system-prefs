bf27f23a6435b78ecdea7c8b6e6f4c257a05836c
chengscott <60510scott@gmail.com>
2025-10-05 04:08:29 +0800

    update

---
+++ utils/etc/profile.d/hf.sh
@@ -0,0 +1,7 @@
+export HF_HUB_DISABLE_TELEMETRY=1
+export HF_HUB_ENABLE_HF_TRANSFER=1
+export HF_DATASETS_CACHE=/data/LLM/huggingface/datasets
+export HF_HUB_CACHE=/data/LLM/huggingface/hub
+export HF_XET_CACHE=/data/LLM/huggingface/xet
+export HF_ASSETS_CACHE=/data/LLM/huggingface/assets
+export NLTK_DATA=/data/LLM/nltk_data
